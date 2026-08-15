cc1aafa041783fd41ea3c913b1090fbaf640b24b
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-15 15:45:32 +0200

    Fix variable names

--- shared-lib/vars/pruneDockerTags.groovy
+++ shared-lib/vars/pruneDockerTags.groovy
@@ -3,7 +3,7 @@ import org.myjenkins.DockerHub
 def call(Map config = [:]) {
     def username = config.username ?: error('pruneDockerTags: username is required')
     def repo = config.repo ?: error('pruneDockerTags: username is required')
-    def userkeepLast = config.userkeepLast ?: 3
+    def keepLast = config.userkeepLast ?: 3
     def credentialsId = config.credentialsId ?: 'dockerhub-credentials'
     def protectedTags = config.protectedTags ?: ['latest']
     
