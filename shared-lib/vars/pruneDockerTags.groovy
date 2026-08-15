6e6054264cfdf5a3133b01a7e6b51b1234a1e172
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-15 16:02:53 +0200

    Move pipeline steps from src to vars

--- shared-lib/vars/pruneDockerTags.groovy
+++ shared-lib/vars/pruneDockerTags.groovy
@@ -1,13 +1,62 @@
 import org.myjenkins.DockerHub
 
 def call(Map config = [:]) {
-    def username = config.username ?: error('pruneDockerTags: username is required')
-    def repo = config.repo ?: error('pruneDockerTags: username is required')
-    def keepLast = config.userkeepLast ?: 3
+    def username      = config.username      ?: error('pruneDockerTags: username is required')
+    def repo          = config.repo          ?: error('pruneDockerTags: repo is required')
+    def keepLast      = config.keepLast      ?: 3
     def credentialsId = config.credentialsId ?: 'dockerhub-credentials'
     def protectedTags = config.protectedTags ?: ['latest']
-    
-    def hub = new DockerHub(this)
-    hub.apiLogin(credentialsId)
-    hub.pruneTags(username, repo, keepLast, protectedTags)
+
+    def apiToken = ''
+
+    // login
+    withCredentials([usernamePassword(
+        credentialsId: credentialsId,
+        usernameVariable: 'DOCKERHUB_USER',
+        passwordVariable: 'DOCKERHUB_TOKEN'
+    )]) {
+        def response = sh(
+            script: '''
+                curl -fsSL -X POST https://hub.docker.com/v2/users/login/ \
+                    -H 'Content-Type: application/json' \
+                    -d '{"username":"'"$DOCKERHUB_USER"'","password":"'"$DOCKERHUB_TOKEN"'"}'
+            ''',
+            returnStdout: true
+        ).trim()
+
+        apiToken = DockerHub.parseToken(response)
+    }
+
+    // get tags
+    def response = sh(
+        script: """
+            curl -fsSL \
+                "https://hub.docker.com/v2/repositories/${username}/${repo}/tags/?page_size=100&ordering=-last_updated" \
+                -H "Authorization: JWT ${apiToken}"
+        """,
+        returnStdout: true
+    ).trim()
+
+    def tags = DockerHub.parseTags(response)
+    echo "Found ${tags.size()} tag(s), keeping ${keepLast} most recent"
+
+    if (tags.size() <= keepLast) {
+        echo "Nothing to delete"
+        return
+    }
+
+    def tagsToDelete = tags[keepLast..-1]
+
+    for (def tag in tagsToDelete) {
+        if (protectedTags.contains(tag.name)) {
+            echo "Skipping protected tag: ${tag.name}"
+            continue
+        }
+        echo "Deleting: ${tag.name}"
+        sh """
+            curl -fsSL -X DELETE \
+                "https://hub.docker.com/v2/repositories/${username}/${repo}/tags/${tag.name}/" \
+                -H "Authorization: JWT ${apiToken}"
+        """
+    }
 }
