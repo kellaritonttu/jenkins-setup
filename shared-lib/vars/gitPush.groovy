d9e7befb2d99a606874641978ae4de1cf24c8866
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-16 17:11:26 +0200

    shared-lib: Fix pushing branch

--- shared-lib/vars/gitPush.groovy
+++ shared-lib/vars/gitPush.groovy
@@ -1,17 +1,19 @@
-import org.myjenkins.Git
-
+// vars/gitPush.groovy
 def call(Map config = [:]) {
     def branch        = config.branch        ?: 'main'
     def repo          = config.repo          ?: error('gitPush: repo is required')
     def credentialsId = config.credentialsId ?: 'github-credentials'
+    
+    def repoHost      = repo.replace('https://', '')
 
     withCredentials([usernamePassword(
         credentialsId: credentialsId,
         usernameVariable: 'GIT_USER',
         passwordVariable: 'GIT_TOKEN'
     )]) {
-        def url = Git.authenticatedUrl(repo, env.GIT_USER, env.GIT_TOKEN)
-        
-        sh "git push ${url} ${branch}"
+        sh """
+            git remote set-url origin https://\${GIT_USER}:\${GIT_TOKEN}@${repoHost}
+            git push origin HEAD:${branch}
+        """
     }
 }
