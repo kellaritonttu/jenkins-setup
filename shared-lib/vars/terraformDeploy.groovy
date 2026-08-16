f431803987b4c347780fb47c6c4731bbfb3295ee
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-16 15:22:18 +0200

    Add Terraform support

---
+++ shared-lib/vars/terraformDeploy.groovy
@@ -0,0 +1,17 @@
+def call(Map config = [:]) {
+    def terraformDir  = config.terraformDir  ?: 'terraform'
+    def imageTag      = config.imageTag      ?: error('terraformDeploy: imageTag is required')
+    def credentialsId = config.credentialsId ?: 'terraform-cloud-token'
+
+    withCredentials([string(
+        credentialsId: credentialsId,
+        variable: 'TF_TOKEN_app_terraform_io'
+    )]) {
+        dir(terraformDir) {
+            sh """
+                terraform init
+                terraform apply -auto-approve
+            """
+        }
+    }
+}
