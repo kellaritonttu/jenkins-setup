2944dbaf08cf554b0ae9545cf5fe7866b229cc79
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-17 01:58:47 +0200

    shared-lib: Fix dockerLogin closure's pattern

--- shared-lib/vars/dockerLogin.groovy
+++ shared-lib/vars/dockerLogin.groovy
@@ -3,12 +3,14 @@ import org.myjenkins.Docker
 def call(Map config = [:]) {
     def credentialsId = config.credentialsId ?: 'dockerhub-credentials'
 
+    def docker = new Docker(this)
+
     withCredentials([usernamePassword(
         credentialsId: credentialsId,
         usernameVariable: 'DOCKER_USER',
         passwordVariable: 'DOCKER_PASS'
     )]) {
-        new Docker(this).login()
+        docker.login()
     }
 
 }
