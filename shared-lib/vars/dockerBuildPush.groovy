8b67ee48f90f352aa8e805af8390f9523c91d7f4
Vafth <nuuskamuikkunen671@gmail.com>
2026-08-16 16:51:52 +0200

    shared-lib: Update dockerBuildPush with Dockerfile context parameter

--- shared-lib/vars/dockerBuildPush.groovy
+++ shared-lib/vars/dockerBuildPush.groovy
@@ -2,8 +2,9 @@ def call(Map config = [:]) {
     def image      = config.image      ?: error('dockerBuild: image is required')
     def tag        = config.tag        ?: error('dockerBuild: tag is required')
     def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context    = config.context    ?: '.'
 
-    sh "docker build -f ${dockerfile} -t ${image}:${tag} ."
+    sh "docker build -f ${dockerfile} -t ${image}:${tag} ${context}"
     sh "docker push ${image}:${tag}"
 
 }
